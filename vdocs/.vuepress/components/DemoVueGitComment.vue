f3ae0058c1df950dcd71f3e0307ee5dab18ab0ed
shalldie <hi.xieshuang@gmail.com>
2021-04-04 01:50:59 +0800

    添加 vuepress

---
+++ vdocs/.vuepress/components/DemoVueGitComment.vue
@@ -0,0 +1,28 @@
+<template>
+    <VueGitComment :options="options" />
+</template>
+
+<script>
+import 'vue-git-comment/dist/vue-git-comment.css';
+import VueGitComment from 'vue-git-comment';
+
+export default {
+    components: {
+        VueGitComment
+    },
+    data() {
+        return {
+            options: {
+                clientID: '6d389de0147fcf7e6d0e',
+                clientSecret: '796a5f4d41b636a4cccd282cbc4adc42f148b39e',
+                owner: 'shalldie',
+                repo: 'shalldie.github.com',
+                uuid: 'for_vue_git_comment',
+                language: 'zh-CN'
+            }
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
